e991ee3e790c54a777d4e04a8965559308757fcc
Raju Islam <bdrajuislam246@gmail.com>
2024-11-26 21:11:59 +0600

    Merged remote README.md with local code

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, () => {
-  console.log("server running in post 3000");
+  console.log("server is running on post 3000");
 });

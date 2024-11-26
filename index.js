e7362e46f6f0d9f905c5e46ec3322b92cd387b14
Raju Islam <bdrajuislam246@gmail.com>
2024-11-26 21:39:21 +0600

    connected to database

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+// utM4zwX8u7nYGpwF
+// bdrajuislam246
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongodb is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 

489419791edb5764070ae1356b684ffb33bc9c6c
Raju Islam <bdrajuislam246@gmail.com>
2024-11-27 11:30:36 +0600

    create logout api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -93,4 +93,19 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { register, login };
+const logout = async (req, res) => {
+  try {
+    res
+      .clearCookie("access_token", {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ message: "Logged out successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error logging out", error: error.message });
+  }
+};
+
+module.exports = { register, login, logout };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth.controller.js");
+const {
+  register,
+  login,
+  logout,
+} = require("../controllers/auth.controller.js");
 
 const router = express.Router();
 
 // Define the register endpoint
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
